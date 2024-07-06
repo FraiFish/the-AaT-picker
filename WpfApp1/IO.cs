83d8f4e6f1d90e329d7d56eefdc2a0a3fb3482b0
geckro <94733731+geckro@users.noreply.github.com>
2024-07-06 21:47:20 +0800

    Split classes into their own files

---
+++ WpfApp1/IO.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+
+namespace WpfApp1;
+
+public class IO
+{
+    // I CANNOT make this static what do you mean VS
+    public Playlist ConvertToPl(String fileName, bool NoEdit)
+    {
+        string path = System.IO.Path.Combine(Directory.GetCurrentDirectory() + "\\songlists\\" + fileName + ".txt");
+        StreamReader stream = new(path);
+        string? line;
+        string allLines = "";
+        int size = 0;
+        NoEditList fuckedUp = new([], "anguishandtorment");
+        try
+        {
+            //Read the first line of text
+            line = stream.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                //write the line to console window
+                allLines += line + "\n";
+                size++;
+                //Read the next line
+                line = stream.ReadLine();
+            }
+            //close the file
+            stream.Close();
+            String[] allSongs = allLines.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            Track[] tracks = new Track[size];
+            /*
+                 turns the string into a playlist.
+                 every line is a song of the format:
+                 name difficulty cc rating composer charter
+                */
+            int incr = 0;
+            foreach (String s in allSongs)
+            {
+                String[] single = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Track cur = new(single[0], single[1], Convert.ToDouble(single[2]), single[3], single[4], single[5], incr);
+                tracks[incr++] = cur;
+            }
+            // TODO: conditional here to create Edit or No Edit list
+            if (NoEdit)
+            {
+                NoEditList returnList = new(tracks, path);
+                return returnList;
+            }
+            else
+            {
+                EditList returnList = new(tracks, path);
+                return returnList;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        // how did you get here
+        return fuckedUp;
+    }
+    // function for deleting a song might have to be in this class...
+}
---
+++ WpfApp1/Track.cs
@@ -0,0 +1,40 @@
+﻿namespace WpfApp1;
+
+public class Track
+{
+    public string trackName;
+    public string trackDiff;
+    public double trackcc;
+    public string trackRating;
+    public string trackComposer;
+    public string? trackCharter;
+    public int trackID;
+    public Track(string name, string diff, double cc, string rating, string composer, string? charter, int id)
+    {
+        trackName = name;
+        trackDiff = diff;
+        trackcc = cc;
+        trackRating = rating;
+        trackComposer = composer;
+        if (charter == null)
+        {
+            trackCharter = null;
+        }
+        else
+        {
+            trackCharter = charter;
+        }
+        trackID = id;
+    }
+    // temporarily removing these because they seemed to be messing with me and I DONT actually know how to define them
+    /*
+        public static bool operator ==(Track one, Track other)
+        {
+            return one.trackID == other.trackID;
+        }
+        public static bool operator !=(Track one, Track other)
+        {
+            return one.trackID != other.trackID;
+        }
+        */
+}
